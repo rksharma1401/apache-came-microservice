367fbc69f9badb505fa1f0f43d03803552dd8a59
Ravikant Sharma <rksharma1401@hotmail.com>
2021-06-06 13:47:08 +0530

    Create ActiveMqReaderRouter.java

---
+++ src/main/java/learn/camelmicorservices/routes/ActiveMqReaderRouter.java
@@ -0,0 +1,14 @@
+package learn.camelmicorservices.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqReaderRouter extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("activemq:my-queue").log("${body}").to("activemq:sec-queue");
+
+	}
+}
