a5366238e9ebabbddc944d9ff41bdcd6af6c8ad5
Ravikant Sharma <rksharma1401@hotmail.com>
2023-07-09 19:30:39 +0530

    Update ActiveMqSenderRouter.java

--- src/main/java/learn/camelmicorservices/routes/ActiveMqSenderRouter.java
+++ src/main/java/learn/camelmicorservices/routes/ActiveMqSenderRouter.java
@@ -10,7 +10,7 @@ public class ActiveMqSenderRouter extends RouteBuilder {
 	public void configure() throws Exception {
 int i=0;
 		from("timer:wss-timer?period=5000").transform().constant("{\"name\":\"new"+(++i)+"\"" )
-			.log("${body}").to("websocket:wss://spring-ou5b.onrender.com/web-socket-endpoint/app/message");
+			.log("${body}").to("websocket:wss://spring-ou5b.onrender.com/web-socket-endpoint");
 
 	}
 }
